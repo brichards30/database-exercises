c566e673e20c6b283e172ff0f71c2ba633261d6d
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-23 09:18:45 -0500

    finished exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,14 +5,7 @@ create table albums (
     artist VARCHAR(50) DEFAULT 'NONE',
     name VARCHAR (50) NOT NULL,
     release_date YEAR UNSIGNED NOT NULL,
-
-)
-
-
-CREATE TABLE quotes (
-                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        author_first_name VARCHAR(50) DEFAULT 'NONE',
-                        author_last_name  VARCHAR(100) NOT NULL,
-                        content TEXT NOT NULL,
-                        PRIMARY KEY (id)
+    sales FLOAT UNSIGNED NOT NULL,
+    genre VARCHAR (20) DEFAULT 'NONE',
+    PRIMARY KEY (id)
 );
