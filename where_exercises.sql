6e893076d92e301b5095da125a362361ec51a0e0
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-24 11:48:30 -0500

    added count(*)

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,31 +1,32 @@
 use employees;
 
 #Part One
-SELECT first_name
+
+SELECT COUNT(*)
 FROM employees.employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-SELECT last_name
+SELECT COUNT(*)
 FROM employees.employees
 WHERE last_name like 'e%';
 
-SELECT last_name
+SELECT COUNT(*)
 FROM employees.employees
 WHERE last_name like '%q%';
 
 #Part Two
-SELECT first_name, gender
+SELECT COUNT(*)
 FROM employees.employees
 WHERE gender = 'M' AND (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya');
 
-SELECT last_name
+SELECT COUNT(*)
 FROM employees.employees
 WHERE last_name like 'e%' or last_name like '%e';
 
-SELECT last_name
+SELECT COUNT(*)
 FROM employees.employees
 WHERE last_name like 'e%e';
 
-SELECT last_name
+SELECT COUNT(*)
 FROM employees.employees
 WHERE last_name not like '%qu%' and last_name like '%q%';
