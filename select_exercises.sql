05c65414e4852fe41b380c2b51a7ce5b573c2aba
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-24 09:28:20 -0500

    used like to get all variations of rock'

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,7 +1,19 @@
 use codeup_test_db;
 
+#All albums by Michael Jackson
 select 'All albums by Michael Jackson', name from albums where artist = 'Michael Jackson';
+
+#Year The Bodyguard was released
 select 'The year The Bodyguard was released', release_date from albums where name = 'The Bodyguard';
+
+#Genre for Nevermind
 select 'The genre(s) for Nevermind is', genre from albums where name = 'Nevermind';
+
+#Albums released in 1990s
 select 'The album(s) released in the 1990s are', name, artist, release_date from albums where release_date BETWEEN 1990 AND 1999;
+
+#Albums less than 20 million certified units
 select 'The album(s) with less than 20 million certified sales', name, artist from albums where sales < 20;
+
+#Albums with genre of rock
+select 'Albums with genre or Rock', name, genre from albums where genre like '%rock%';
