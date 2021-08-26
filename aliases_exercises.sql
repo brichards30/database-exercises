8b278eb787468a7ba3fbc87357cd2b5eccb3bb65
Brittany Richards <brittany.m.richards3@gmail.com>
2021-08-26 09:48:53 -0500

    formatting edits

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,5 +1,5 @@
 use employees;
 
-SELECT CONCAT(emp_no, ' - ',first_name, ' ', last_name) AS full_name, birth_date as DOB
+SELECT CONCAT(emp_no, ' - ',last_name, ', ', first_name) AS full_name, birth_date as DOB
 FROM employees
 LIMIT 10;
